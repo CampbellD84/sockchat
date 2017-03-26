afcc7e943100fa1260799a80a739b4c346fe3fc7
Douglas Campbell <dacampbelltbone@gmail.com>
2017-03-26 03:39:26 -0400

    set up Express server

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,10 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import io from 'socket.io-client';
+import MessageList from './messages/MessageList.jsx';
+import MessageForm from './messages/MessageForm.jsx';
+import UserList from './users/UserList.jsx';
+import UserForm from './users/UserForm.jsx';
 
 class App extends Component {
   constructor(props){
@@ -9,10 +14,26 @@ class App extends Component {
     }
   }
 
+  componentWillMount() {
+    this.socket = io('http://localhost:3000');
+    this.socket.on('connect', this.connect.bind(this));
+  }
+
+  connect(){
+    this.setState({status: 'connected'});
+    console.log('Connected: ' + this.socket.id);
+  }
+
   render(){
     return(
-      <div>
-        This is my app
+      <div className="row">
+        <div className="col-md-4">
+          <UserList />
+        </div>
+        <div className="col-md-8">
+          <MessageList />
+          <MessageForm />
+        </div>
       </div>
     )
   }
